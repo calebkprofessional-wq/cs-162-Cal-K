2e81a5c07f15f8cc7ea6a3b28c166f54285d98dd
Caleb Kitchens <calebkprofessional@gmail.com>
2026-04-05 13:16:29 -0700

    Comments added

--- caleb_kitchens.py
+++ caleb_kitchens.py
@@ -1,7 +1,7 @@
 #caleb_kitchens.py
 #Written by Caleb Kitchens
 #Function: rect_surface_area
-def rect_surface_are(length, width, height):
+def rect_surface_area(length, width, height):
     total = rect_area(length, height) * 4
     total += rect_area(height, width) * 2
     return total
