7a95b51f0bc2205178c310e5fcc7c8b634262633
Caleb Kitchens <calebkprofessional@gmail.com>
2026-04-12 14:32:43 -0700

    Week 2 Project finished

---
+++ Week 2 Pie Chart.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plot
+
+# set up your lists
+
+numlist = [8, 6, 5, 3]
+
+namelist = ['freshmen', 'sophomores', 'juniors', 'seniors']
+
+colorlist = ['blue', 'green', 'pink', 'white' ]
+
+explodelist = [0.5, 0.1, 0.0, 0.0]
+
+# make the pie chart
+plot.pie(numlist, labels=namelist, autopct='%d%%', colors=colorlist, explode = explodelist, startangle = 200)
+plot.axis('equal')
+plot.savefig('piechart3.png')
